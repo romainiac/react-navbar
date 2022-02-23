09d9bba59829ac1b73a1025a6e03f7a4a6c3384f
unknown <romanyefimets@gmail.com>
2022-02-23 10:46:38 -0800

    menu items can be toggled

--- src/navbar.component.jsx
+++ src/navbar.component.jsx
@@ -1,19 +1,36 @@
 import { FaBars, FaTwitter } from 'react-icons/fa';
 import logo from './logo.svg';
 import { links, social } from './data.js';
-
+import { useState, useRef, useEffect } from 'react';
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="logo" />
-          <button className="nav-toggle">
+          <button
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
-        <div className="links-container show-container">
-          <ul className="links">
+
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map((link) => {
               const { id, url, text } = link;
               return (
@@ -24,6 +41,7 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+
         <ul className="social-icons">
           {social.map((item) => {
             const { url, icon, id } = item;
